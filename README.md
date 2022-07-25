e723777824eb263e242a0f411cabba34795ffb9d
geoffrey <geoffrey.maillot@gmail.com>
2022-07-25 15:42:15 +0200

    Done

--- README.md
+++ README.md
@@ -1,46 +1,76 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# React-hook-form_Yup
 
-## Available Scripts
 
-In the project directory, you can run:
 
-### `npm start`
+## 📖 Lesson learned
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+- react-hook-form
+- yup 
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+## 🛠 Stack
 
-### `npm test`
+- React
+- Gestion Formulaire
+   - react-hook-form
+   - yup
+   - yup-password
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+## Screenshots
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+![App Screenshot](./2022-07-25_15h38_06.png)
+![App Screenshot](./2022-07-25_15h41_20.png)
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+## Run Locally
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
-## Learn More
+Clone project
+```bash
+git clone git@github.com:Projet
+```
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+Go to folder directory
+```bash
+cd Projet
+```
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+
+Install dependencies
+```bash
+# yarn
+yarn
+
+# npm
+npm install
+```
+
+Start server
+```bash
+# yarn
+yarn start
+
+# npm
+npm start
+
+# Projet run on http://localhost:3000/
+```
+
+
+
+
+
+## prerequisites
+
+- Node.js
+
+## 🔗 Links
+
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://maillot-geoffrey-portfolio.xyz/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/geoffrey-maillot-06a1411bb/)
+[![linkedin](https://img.shields.io/badge/github-24292F?style=for-the-badge&logo=github&logocolor=white)](https://github.com/Geoffrey-Maillot/)
---
+++ src/Form.tsx
@@ -0,0 +1,241 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './Form.scss';
+
+// Import MUI
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+// React icon
+import { BiHide } from 'react-icons/bi';
+import { BiShow } from 'react-icons/bi';
+
+// Import hook form
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import YupPassword from 'yup-password';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+YupPassword(yup);
+
+interface Inputs {
+  lastName: string;
+  firstName: string;
+  email: string;
+  confirmEmail: string;
+  password: string;
+  confirmPassword: string;
+  adress: string;
+  zipCode: number;
+  city: string;
+  termsOfUse: boolean;
+}
+
+const schema = yup.object({
+  lastName: yup
+    .string()
+    .min(2, 'Il doit y avoir au moins deux lettres')
+    .required('Ce champ est obligatoire'),
+  firstName: yup
+    .string()
+    .min(2, 'Il doit y avoir au moins deux lettres')
+    .required('Ce champ est obligatoire'),
+  email: yup
+    .string()
+    .email('Veuillez rentrer une adresse email valide')
+    .required('Ce champ est obligatoire'),
+  confirmEmail: yup
+    .string()
+    .oneOf([yup.ref('email')], 'Les emails doivent correspondrent ')
+    .required('Ce champ est obligatoire'),
+  password: yup
+    .string()
+    .min(8, 'Le mot de passe doit contenir au moins 8 caractère')
+    .minLowercase(1, 'Le mot de passe doit contenir au moins une minuscule')
+    .minUppercase(1, 'Le mot de passe doit contenir au moins une majuscule')
+    .minNumbers(1, 'Le mot de passe doit contenir au moins un nombre')
+    .minSymbols(
+      1,
+      'Le mot de passe doit contenir au moins un caractère spécial'
+    )
+    .required(),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Les mots de passe doivent correcpondre')
+    .required('Ce champ est obligatoire'),
+  adress: yup.string().required('Ce champ est obligatoire'),
+  zipCode: yup
+    .string()
+    .length(5, 'Le code postal doit contenir 5 chiffres')
+    .matches(/^[0-9]+$/, 'Le code postal ne peu contenir que des chiffres')
+
+    .required('Ce champ est obligatoire'),
+  city: yup.string().required('Ce champ est obligatoire'),
+  termsOfUse: yup
+    .bool()
+    .oneOf([true], "Veuillez accepter les conditions d'utilisation")
+    .required('Ce champ est obligatoire'),
+});
+
+function App() {
+  const [passwordIsVisible, togglePasswordIsVisible] = useState<boolean>(false);
+  const [confirmPasswordIsVisible, toggleConfirmPasswordIsVisible] =
+    useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+    mode: 'onTouched',
+  });
+
+  const wait = function () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve('Success'), 1000);
+    });
+  };
+
+  const onSubmit = async (data: Inputs) => {
+    console.log(data);
+    console.log(formState);
+    await wait();
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          error={!!errors.lastName}
+          helperText={errors.lastName?.message}
+          fullWidth
+          label="Votre nom"
+          {...register('lastName')}
+        />
+        <TextField
+          error={!!errors.firstName}
+          helperText={errors.firstName?.message}
+          fullWidth
+          label="Votre Prénom"
+          {...register('firstName')}
+        />
+        <TextField
+          error={!!errors.email}
+          helperText={errors.email?.message}
+          fullWidth
+          label="Votre Email"
+          {...register('email')}
+        />
+        <TextField
+          error={!!errors.confirmEmail}
+          helperText={errors.confirmEmail?.message}
+          fullWidth
+          label="Confirmer l'email"
+          {...register('confirmEmail')}
+        />
+        <span className="wrap-input">
+          <TextField
+            error={!!errors.password}
+            helperText={errors.password?.message}
+            fullWidth
+            label="Mot de passe"
+            {...register('password')}
+            type={passwordIsVisible ? 'text' : 'password'}
+          />
+          <button
+            className="buttonPassword"
+            type="button"
+            onClick={() =>
+              togglePasswordIsVisible((passwordIsVisible) => !passwordIsVisible)
+            }
+          >
+            {passwordIsVisible ? (
+              <BiHide size={'1.4rem'} />
+            ) : (
+              <BiShow size={'1.4rem'} />
+            )}
+          </button>
+        </span>
+        <span className="wrap-input">
+          <TextField
+            error={!!errors.confirmPassword}
+            helperText={errors.confirmPassword?.message}
+            fullWidth
+            label="Confirmer le mot de passe"
+            {...register('confirmPassword')}
+            type={confirmPasswordIsVisible ? 'text' : 'password'}
+          />
+          <button
+            className="buttonPassword"
+            type="button"
+            onClick={() =>
+              toggleConfirmPasswordIsVisible(
+                (confirmPasswordIsVisible) => !confirmPasswordIsVisible
+              )
+            }
+          >
+            {confirmPasswordIsVisible ? (
+              <BiHide size={'1.4rem'} />
+            ) : (
+              <BiShow size={'1.4rem'} />
+            )}
+          </button>
+        </span>
+        <TextField
+          error={!!errors.adress}
+          helperText={errors.adress?.message}
+          fullWidth
+          label="Adresse"
+          {...register('adress')}
+        />
+        <TextField
+          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+          error={!!errors.zipCode}
+          helperText={errors.zipCode?.message}
+          fullWidth
+          label="Code Postal"
+          {...register('zipCode')}
+        />
+        <TextField
+          error={!!errors.city}
+          helperText={errors.city?.message}
+          fullWidth
+          label="Ville"
+          {...register('city')}
+        />
+
+        <div className="checkbox">
+          <FormControlLabel
+            control={<Checkbox defaultChecked {...register('termsOfUse')} />}
+            label="J'accepte les conditions d'utilisation"
+          />
+
+          {!!errors.termsOfUse && (
+            <p style={{ color: 'red' }}>{errors.termsOfUse.message}</p>
+          )}
+        </div>
+
+        <Button
+          className="send"
+          size="large"
+          color="primary"
+          variant="contained"
+          type="submit"
+          disabled={isSubmitting}
+        >
+          Envoyer
+        </Button>
+        {isSubmitSuccessful && <p className='messageSuccess'>Le formulaire a bien été soumis</p>}
+      </form>
+    </div>
+  );
+}
+
+export default App;
