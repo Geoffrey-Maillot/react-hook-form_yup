c2b2d7923be2565b996242d216ca8a9581fc9e9b
Geoffrey Maillot <geoffrey.maillot@lomaco.fr>
2022-07-25 06:16:00 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function App() {
+  const errors = null;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <form action="" className="form">
+        <TextField
+          fullWidth
+          name="lastName"
+          label="Votre nom"
+        />
+        <TextField fullWidth name="FirstName" label="Votre Prénom" />
+        <TextField fullWidth name="email" label="Votre Email" />
+        <TextField fullWidth name="confirmEmail" label="Confirmer l'email" />
+        <TextField fullWidth name="password" label="Mot de passe" />
+        <TextField
+          fullWidth
+          name="confirmPassword"
+          label="Confirmer le mot de passe"
+        />
+        <TextField fullWidth name="adress" label="Adresse" />
+        <TextField fullWidth name="zipCode" label="Code Postal" />
+        <TextField fullWidth name="city" label="Ville" />
+
+        <Button size="large" color="primary" variant="contained">
+          Envoyer
+        </Button>
+      </form>
     </div>
   );
 }
